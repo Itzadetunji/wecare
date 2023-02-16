9039b95ae0fdf69ae33930f6d7db5d5b3bbc588f
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2023-02-16 16:39:35 +0100

    Feat: Cleaned up unused files and code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,7 @@ module.exports = {
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
-	theme: {
-		extend: {
-			screens: {},
-			colors: { darkblue: { 100: "#082161" } },
-			animation: {
-				text: "text 8s ease infinite",
-			},
-			keyframes: {
-				text: {
-					"0%, 100%": {
-						"background-size": "200% 200%",
-						"background-position": "left center",
-					},
-					"50%": {
-						"background-size": "200% 200%",
-						"background-position": "right center",
-					},
-				},
-			},
-		},
-	},
+	theme: {},
 	variants: {
 		extend: {},
 	},
