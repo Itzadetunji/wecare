2d4c533d2c5c524faaaf2f5bc38116213201fcf8
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-02-28 10:40:07 +0100

    Added the HeroText section to the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import type { NextPage } from "next";
+import HeroText from "@/components/HeroText";
 
 const Home: NextPage = () => {
-	return <>We Care</>;
+	return (
+		<main>
+			<HeroText />
+		</main>
+	);
 };
 export default Home;
