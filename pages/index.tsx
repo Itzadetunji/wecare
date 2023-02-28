336b0852f33c13e3f287419dbbdfe64938108036
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-02-28 16:11:35 +0100

    Added the hero image section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import type { NextPage } from "next";
 import HeroText from "@/components/HeroText";
+import HeroImage from "@/components/HeroImage";
 
 const Home: NextPage = () => {
 	return (
 		<main>
+			<section className="bg-[#FDFAFF]">
 			<HeroText />
+			<HeroImage />
+
+			</section>
 		</main>
 	);
 };
